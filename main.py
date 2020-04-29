a8d331b35b4dc48cc1b5fcd451577d8b6c65e1ac
Viggy Kumaresan <viggykumaresan@gmail.com>
2020-04-29 17:27:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import pickle
 import xgboost as xgb
 import gcsfs
 import math
+import os
 
-app = Flask(__name__)
+pd.options.display.float_format = '{:,}'.format
 
 # Load data and model
 fs = gcsfs.GCSFileSystem(project='box-office-revenue-prediction')
@@ -17,11 +18,18 @@ with fs.open('box-office-revenue-prediction/features_test.csv') as f:
 with fs.open('box-office-revenue-prediction/revenue.csv') as f:
     revenue = pd.read_csv(f)
 
+
+# features_train = pd.read_csv('../features_train.csv')
+# features_test = pd.read_csv('../features_test.csv')
+# revenue = pd.read_csv('../revenue.csv')
+
 model = pickle.load(open('model.pkl', 'rb'))
-# features_train = pd.read_csv('features_train.csv')
-# features_test = pd.read_csv('features_test.csv')
-# revenue = pd.read_csv('revenue.csv')
 revenue.rename(columns={'Unnamed: 0': 'index'}, inplace=True)
+
+TRAIN_FOLDER = 'train/'
+TEST_FOLDER = 'test/'
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -42,15 +50,129 @@ def predict():
         revenue_subset = revenue[revenue['index'] == int(features_subset['index'])]
         actual_revenue = math.exp(revenue_subset['revenue'].values[0])
 
+        # poster
+        path = str(str(features_subset['id'].values[0]) + '.jpeg')
+        poster = os.path.join(TRAIN_FOLDER, path)
+
+        # Budget
+        budget = "${:,}".format(features_subset['budget'].values[0])
+
+        # Release Date
+        release_date =features_subset['release_date'].values[0]
+
+        # Runtime
+        runtime = features_subset['runtime'].values[0]
+
+        # Tagline
+        tagline =features_subset['tagline'].values[0]
+
         # Drop string features
-        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0'], axis=1, inplace=True)
+        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0', 'id', 'tagline', 'release_date', 'Unnamed: 0.1'], axis=1, inplace=True)
 
         # Predict
         predicted_revenue = math.exp(model.predict(xgb.DMatrix(features_subset.values))[0])
 
         # Round
-        actual_revenue = round(actual_revenue, 2)
-        predicted_revenue = round(predicted_revenue, 2)
+        actual_revenue = round(actual_revenue)
+        actual_revenue = "\n ${:,}".format(actual_revenue)
+        predicted_revenue = round(predicted_revenue)
+        predicted_revenue = "\n ${:,}".format(predicted_revenue)
+
+    elif movie_name in movie_names_test:
+        # subset dataframes
+        features_subset = features_test[features_test['title'] == movie_name]
+        revenue_subset = revenue[revenue['index'] == int(features_subset['index'])]
+        actual_revenue = '\nNot found in dataset'
+
+        # poster
+        path = str(str(features_subset['id'].values[0]) + '.jpeg')
+        poster = os.path.join(TEST_FOLDER, path)
+
+        # Budget
+        budget = "${:,}".format(features_subset['budget'].values[0])
+
+        # Release Date
+        release_date =features_subset['release_date'].values[0]
+
+        # Runtime
+        runtime = features_subset['runtime'].values[0]
+
+        # Tagline
+        tagline =features_subset['tagline'].values[0]
+
+        # Drop string features
+        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0', 'id', 'tagline', 'release_date', 'Unnamed: 0.1'], axis=1, inplace=True)
+
+        # Predict
+        predicted_revenue = math.exp(model.predict(xgb.DMatrix(features_subset.values))[0])
+
+        # Round
+        #actual_revenue = round(actual_revenue, 2)
+        predicted_revenue = round(predicted_revenue)
+        predicted_revenue = "\n ${:,}".format(predicted_revenue)
+
+    else:
+        actual_revenue = ''
+        predicted_revenue = ''
+        poster = 'movie_theatre.jpeg'
+        movie_name = '{} not found'.format(movie_name)
+        budget=''
+        release_date=''
+        runtime=''
+        tagline=''
+
+    return (render_template('index.html',
+        movie_text=movie_name,
+        budget_text='Budget: {}'.format(budget),
+        release_date_text='Release Date: {}'.format(release_date),
+        runtime_text='Runtime (minutes): {}'.format(runtime),
+        tagline_text=tagline,
+        actual_text='Actual Box Office Revenue: {}'.format(actual_revenue),
+        prediction_text='Predicted Box Office Revenue: {}'.format(predicted_revenue),
+        user_image = poster))
+
+@app.route('/results',methods=['POST'])
+def results():
+    movie_names = [x for x in request.form.values()]
+    movie_name = movie_names[0]
+
+    # get list of unique movie names in train and test
+    movie_names_train = list(features_train['title'].unique())
+    movie_names_test = list(features_test['title'].unique())
+
+    if movie_name in movie_names_train:
+        # subset dataframes
+        features_subset = features_train[features_train['title'] == movie_name]
+        revenue_subset = revenue[revenue['index'] == int(features_subset['index'])]
+        actual_revenue = math.exp(revenue_subset['revenue'].values[0])
+
+        # poster
+        path = str(str(features_subset['id'].values[0]) + '.jpeg')
+        poster = os.path.join(TRAIN_FOLDER, path)
+
+        # Budget
+        budget = "${:,}".format(features_subset['budget'].values[0])
+
+        # Release Date
+        release_date =features_subset['release_date'].values[0]
+
+        # Runtime
+        runtime = features_subset['runtime'].values[0]
+
+        # Tagline
+        tagline =features_subset['tagline'].values[0]
+
+        # Drop string features
+        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0', 'id', 'tagline', 'release_date', 'Unnamed: 0.1'], axis=1, inplace=True)
+
+        # Predict
+        predicted_revenue = math.exp(model.predict(xgb.DMatrix(features_subset.values))[0])
+
+        # Round
+        actual_revenue = round(actual_revenue)
+        actual_revenue = "${:,}".format(actual_revenue)
+        predicted_revenue = round(predicted_revenue)
+        predicted_revenue = "${:,}".format(predicted_revenue)
 
     elif movie_name in movie_names_test:
         # subset dataframes
@@ -58,31 +180,45 @@ def predict():
         revenue_subset = revenue[revenue['index'] == int(features_subset['index'])]
         actual_revenue = 'Not found in dataset'
 
+        # poster
+        path = str(str(features_subset['id'].values[0]) + '.jpeg')
+        poster = os.path.join(TEST_FOLDER, path)
+
+        # Budget
+        budget = "${:,}".format(features_subset['budget'].values[0])
+
+        # Release Date
+        release_date =features_subset['release_date'].values[0]
+
+        # Runtime
+        runtime = features_subset['runtime'].values[0]
+
+        # Tagline
+        tagline =features_subset['tagline'].values[0]
+
         # Drop string features
-        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0'], axis=1, inplace=True)
+        features_subset.drop(['title', 'imdb_id', 'index', 'Unnamed: 0', 'id', 'tagline', 'release_date', 'Unnamed: 0.1'], axis=1, inplace=True)
 
         # Predict
         predicted_revenue = math.exp(model.predict(xgb.DMatrix(features_subset.values))[0])
 
         # Round
         #actual_revenue = round(actual_revenue, 2)
-        predicted_revenue = round(predicted_revenue, 2)
+        predicted_revenue = round(predicted_revenue)
+        predicted_revenue = "${:,}".format(predicted_revenue)
 
     else:
-        actual_revenue = 'Movie data not found'
-        predicted_revenue = 'Movie data not found'
+        actual_revenue = ''
+        predicted_revenue = ''
+        poster = 'movie_theatre.jpeg'
+        movie_name = '{} not found'.format(movie_name)
+        budget=''
+        release_date=''
+        runtime=''
+        tagline=''
 
-    return (render_template('index.html', actual_text='Actual Box Office Revenue (Millions): $ {}'.format(actual_revenue),
-        prediction_text='Predicted Box Office Revenue (Millions): $ {}'.format(predicted_revenue)))
-
-# @app.route('/results',methods=['POST'])
-# def results():
-#
-#     data = request.get_json(force=True)
-#     prediction = model.predict([np.array(list(data.values()))])
-#
-#     output = prediction[0]
-#     return jsonify(output)
+    output = { 'predicted_revenue': predicted_revenue, 'actual_revenue': actual_revenue }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080) # host='0.0.0.0'
